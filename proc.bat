b0ced93164d88ccbbbb5fc2ad51bed6e827b05ec
NOTfedos <mistkap@gmail.com>
2021-03-29 07:23:03 +0300

    first version

---
+++ proc.bat
@@ -0,0 +1,5 @@
+g++ process.cpp -lgmpxx -lgmp -o proc.exe
+pause
+proc.exe
+pause
+cls
---
+++ process.cpp
@@ -0,0 +1,168 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <ctime>
+
+#include <gmpxx.h>
+
+
+using std::vector;
+using std::rand;
+using std::pow;
+using std::cout;
+using std::ofstream;
+using std::endl;
+using std::string;
+
+
+const unsigned long N = pow(10, 6);
+const unsigned int A = 1, B = 1, C = 1;
+const unsigned int COUNT_LIMIT = pow(10, 2);
+const mpf_class dt = pow(10.0, -4);
+const mpf_class v_c = pow(10.0, -3);
+const double M = pow(10, -10);
+
+
+string output_filename = "data.txt";
+
+
+class Body{
+public:
+    mpf_class x, y, z, px, py, pz;
+
+    Body(){};
+
+    Body(const double& a, const double& b, const double& c){
+        x = rand() / RAND_MAX * a;
+        y = rand() / RAND_MAX * b;
+        z = rand() / RAND_MAX * c;
+        px = rand();
+        py = rand();
+        pz = rand();  // распределение может быть не Максвелловским
+    }
+
+    // обновление координат
+    void move(){
+        this->x += px / M * dt;
+        this->y += py / M * dt;
+        this->z += pz / M * dt;
+    }
+};
+
+class BoxContainer{
+// параллелограм в начале координат
+public:
+    mpf_class a, b, c;
+
+    BoxContainer(double a, double b, double c){
+        this->a = a;  // по X координате
+        this->b = b;  // по Y координате
+        this->c = c;  // по Z координате
+    }
+
+    mpf_class get_volume() const{
+        return a * b * c;
+    }
+
+    mpf_class get_square() const{
+        return 2*(a*b + b*c + a*c);
+    }
+
+    mpf_class collide(Body& molecula) const{
+        mpf_class is_collide = 0;
+        if ((molecula.x < 0) || (molecula.x > a)){
+            molecula.px = -1 * molecula.px;
+            is_collide += 2 * abs(molecula.px) / dt;
+        }
+        if ((molecula.y < 0) || (molecula.y > b)){
+            molecula.py = -1 * molecula.py;
+            is_collide = 2 * abs(molecula.py) / dt;
+        }
+        if ((molecula.z < 0) || (molecula.z > c)){
+            molecula.pz = -1 * molecula.pz;
+            is_collide = 2 * abs(molecula.pz) / dt;
+        }
+        return is_collide;
+    }
+
+};
+
+
+
+template<class T> class MyVector{  // неизменяемый вектор
+public:
+    MyVector(unsigned int n=0){
+        this->arr = new T[n];
+        this->self_size = n;
+    }
+
+    void set_at(T& x, const unsigned int& i){
+        if ((i < 0) || (i>=this->self_size)){
+            return ;
+        }
+        this->arr[i] = x; 
+    }
+
+    Body& get_at(const unsigned int& i){
+        return this->arr[i];
+    }  
+
+    T& operator[](int i){
+        return this->get_at(i);
+    }
+
+    void size() const{
+        return this->self_size;
+    }
+
+private:
+    unsigned int self_size;
+    T *arr;
+};
+
+
+MyVector<Body> spawn_molecules(){
+    MyVector<Body> arr(N);
+    for (int i=0; i < N; i++){
+        arr[i] = Body(A, B, C);
+    }
+    return arr;
+}
+
+
+int main(){
+
+    long int start = clock();
+
+    ofstream outf;
+    outf.open(output_filename);
+
+    BoxContainer box = BoxContainer(A, B, C);
+    MyVector<Body> arr = spawn_molecules();//
+
+    long int counter = 0;
+    while(counter < COUNT_LIMIT){
+        mpf_class collides = 0;
+        for (unsigned long i=0; i < N; i++){
+            arr[i].move();
+            collides += box.collide( arr[i] );
+        }
+
+        outf << collides / box.get_square() << " " <<  box.get_volume() << endl;  // вывод данных
+
+        // отладочный вывод
+        if (counter % (COUNT_LIMIT / 30) == 0){
+            cout << collides / box.get_square() << " " << box.get_volume() << endl;
+        }
+        box.c += dt * v_c;
+        counter++;
+    }
+
+    long int end = clock();
+    cout << "Calc ended in " << (end - start) / CLOCKS_PER_SEC << endl;
+
+    outf.close();
+
+    return 0;
+}
